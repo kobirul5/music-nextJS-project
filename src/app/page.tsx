33d022ef54e886e57c955d8f06e0643417b3f0be
kobirul5 <kobirul05j@gmail.com>
2025-01-30 15:26:15 +0600

    compllet nav design and fixed home nav section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@
 export default function Home() {
   return (
     <div>
-      <h1>hello word</h1>
+      <main className="min-h-screen bg-black/[0.9] antialiased text-white">
+      <h1 className="text-2xl text-center pt-40">hello</h1>
+      </main>
     </div>
   );
 }
